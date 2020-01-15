dbc50f9f5274b04fe9c57dff728ca35b7b2b6985
sadb1312s <andreyalyshev52@gmail.com>
2020-01-15 21:38:19 +0300

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            Connection mySql = DriverManager.getConnection("localhost:3306", "root", "123456");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        launch(args);
+    }
+}
