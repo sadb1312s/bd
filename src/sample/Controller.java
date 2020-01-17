b9adc1a6af051638acba8aa81d15792e2221cb9b
sadb1312s <andreyalyshev52@gmail.com>
2020-01-17 22:23:25 +0300

    динамичиский размер таблицы

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,6 +12,7 @@ import sample.Data.Data3;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class Controller {
@@ -45,11 +46,11 @@ public class Controller {
     private Pane tablePane;
 
 
+    public static String currentTable;
     myDBworker dBWorker;
 
     public void initialize() {
         dBWorker = new myDBworker();
-
 
         connectButton.setOnAction(actionEvent -> {
             try {
@@ -78,14 +79,17 @@ public class Controller {
             }
 
         });
+
         typeButtun.setOnAction(actionEvent -> {
-            ArrayList<ArrayList<String>> data = dBWorker.sendSQLselectAllRequest("goodstype");
+            currentTable = "goodstype";
+            TableView data = dBWorker.sendSQLselectAllRequest(currentTable);
             addGrid(data);
 
 
         });
         goodsButton.setOnAction(actionEvent -> {
-            ArrayList<ArrayList<String>> data = dBWorker.sendSQLselectAllRequest("goods");
+            currentTable = "goods";
+            TableView  data = dBWorker.sendSQLselectAllRequest("goods");
             addGrid(data);
         });
 
@@ -103,104 +107,12 @@ public class Controller {
         goodsreceiptButton.setDisable(isActive);
         suppliersButton.setDisable(isActive);*/
     }
-    public void addGrid(ArrayList<ArrayList<String>> data){
-
+    public void addGrid(TableView data){
         tablePane.getChildren().clear();
-
-        int size = data.get(0).size();
-        System.out.println("размер таблицы = "+size);
-
-
-
-        ObservableList<Data3> tData = FXCollections.observableArrayList();
-        //добавление данных в таблицу
-        for(int i = 1; i < data.size(); i++){
-            ArrayList<String> o = data.get(i);
-            Data3 t = new Data3(o);
-            tData.add(t);
-        }
-
-
-        //column add
-        TableView<Data3> tableView = new TableView<>(tData);
-        tableView.setEditable(true);
-        tableView.setPrefWidth(600);
-        for(int i = 1; i < size + 2; i++){
-            TableColumn<Data3, String> dataColumn;
-            //данные
-            if(i < size) {
-                dataColumn = new TableColumn<Data3, String>(data.get(0).get(i));
-                dataColumn.setCellValueFactory(new PropertyValueFactory<Data3, String>("data" + (i + 1)));
-                dataColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-
-                dataColumn.setOnEditCommit(dataStringCellEditEvent -> {
-
-                });
-
-                tableView.getColumns().add(dataColumn);
-
-            }
-            //служебные столбцы редактировать и удалить
-            if(i == size){
-                dataColumn = new TableColumn<Data3, String>();
-                //dataColumn.setCellValueFactory(new PropertyValueFactory<Data3, String>("dataE"));
-                dataColumn.setCellFactory(getButton("изменить",1,1));
-                tableView.getColumns().add(dataColumn);
-            }
-            if(i == size + 1){
-                dataColumn = new TableColumn<Data3, String>();
-                //dataColumn.setCellValueFactory(new PropertyValueFactory<Data3, String>("dataD"));
-                dataColumn.setCellFactory(getButton("удалить",2,2));
-                tableView.getColumns().add(dataColumn);
-            }
-        }
-
-
-        tablePane.getChildren().addAll(tableView);
-
+        tablePane.getChildren().addAll(data);
     }
 
-    //засунуть кнопку в ячейку таблицы, не пытаться понять как это работает
-    public Callback<TableColumn<Data3, String>, TableCell<Data3, String>> getButton(String name,int id,int type){ //type = 1 едит, 2 удалить
-        Callback<TableColumn<Data3, String>, TableCell<Data3, String>> cellFactory
-                = //
-                new Callback<TableColumn<Data3, String>, TableCell<Data3, String>>() {
-                    @Override
-                    public TableCell call(final TableColumn<Data3, String> param) {
-                        final TableCell<Data3, String> cell = new TableCell<Data3, String>() {
 
-                            final Button btn = new Button(name);
-
-                            @Override
-                            public void updateItem(String item, boolean empty) {
-                                super.updateItem(item, empty);
-                                if (empty) {
-                                    setGraphic(null);
-                                    setText(null);
-                                } else {
-                                    btn.setOnAction(event -> {
-                                        Data3 cellData3 = getTableView().getItems().get(getIndex());
-                                        System.out.println("нажали кнопку id : "+cellData3.id);
-                                        if(type == 1){
-                                            System.out.println("нужно отредактировать");
-                                        }
-                                        if(type == 2){
-                                            System.out.println("нужно удалить "+cellData3.id);
-                                            System.out.println(dBWorker);
-                                            dBWorker.SQLRequest("delete from goodstype where id = "+cellData3.id+";");
-                                        }
-
-                                    });
-                                    setGraphic(btn);
-                                    setText(null);
-                                }
-                            }
-                        };
-                        return cell;
-                    }
-                };
-        return cellFactory;
-    }
 
 }
 
--- src/sample/myDBworker.java
+++ src/sample/myDBworker.java
@@ -1,5 +1,17 @@
 package sample;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.Callback;
+import sample.Data.Data3;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,68 +42,160 @@ public class myDBworker {
             }
         }
     }
-    public ArrayList<ArrayList<String>> sendSQLselectAllRequest(String tableName){
-        ArrayList<ArrayList<String>> data = new ArrayList<>();
+    public TableView sendSQLselectAllRequest(String tableName){
+
+        TableView tableView = new TableView();
+        //tableView.setMaxWidth(650);
+        tableView.setEditable(true);
+        ObservableList<ObservableList> data = FXCollections.observableArrayList();
+
         try {
             System.out.println("посылаем запрос таблицы :" +tableName);
-            rs = stmt.executeQuery("select * from "+tableName+" where id = 1;");
+            rs = stmt.executeQuery("select * from "+tableName+";");
 
             //определяем размер таблицы
-            int size = 0;
-            while (rs.next()) {
-                int i = 1;
-                try {
-
-                    for(;;i++){
-                        rs.getString(i);
-                        System.out.println(">> "+rs.getString(i));
-                    }
-
-                }catch (Exception e){
-                    //e.printStackTrace();
-                    size = i - 1;
-
-                }
-            }
+            int size = rs.getMetaData().getColumnCount();
 
             System.out.println("размер таблицы = "+size);
 
-            //названия столбцов
-            data.add(new ArrayList<>());
-            rs = stmt.executeQuery("desc "+tableName+";");
-            while (rs.next()) {
-                //System.out.print(rs.getString(1)+" ");
-                data.get(0).add(rs.getString(1));
-            }
-            //System.out.println();
+            System.out.println("Столбцы");
+            /**
+             * ********************************
+             * TABLE COLUMN ADDED DYNAMICALLY *
+             *********************************
+             **/
+            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                //We are using non property style for making dynamic table
+                final int j = i;
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
 
-            //данные
-            rs = stmt.executeQuery("select * from "+tableName);
-            while (rs.next()) {
-                data.add(new ArrayList<>());
-                for(int i = 0; i < size; i++){
-                    //System.out.print(rs.getString(i+1)+" ");
-                    data.get(data.size() - 1).add(rs.getString(i+1));
+                col.setCellFactory(TextFieldTableCell.forTableColumn());
+                col.setOnEditCommit(dataStringCellEditEvent -> {
+
+                });
+
+                col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                    public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                        return new SimpleStringProperty(param.getValue().get(j).toString());
+                    }
+                });
+
+                tableView.getColumns().addAll(col);
+                System.out.println("Column [" + i + "] ");
+
+            }
+            //для кнопок редактирования и удаления
+            TableColumn col = new TableColumn();
+            col.setCellFactory(getButtonCell("изменить"));
+
+            TableColumn col2 = new TableColumn();
+            col2.setCellFactory(getButtonCell("удалить"));
+
+            col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                    return new SimpleStringProperty(param.getValue().get(size).toString());
                 }
-                //System.out.println();
+            });
+            col2.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                    return new SimpleStringProperty(param.getValue().get(size + 1).toString());
+                }
+            });
+
+            tableView.getColumns().addAll(col,col2);
+
+            System.out.println("Данные");
+            /**
+             * ******************************
+             * Data added to ObservableList *
+             *******************************
+            */
+            while (rs.next()) {
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+
+                row.add("изменить");
+                row.add("удалить");
+                System.out.println("Row [1] added " + row);
+                data.add(row);
+
+
             }
+            tableView.setItems(data);
 
 
-            /*for(ArrayList<String> e : data) {
-                for (String s : e)
-                    System.out.print(s + " ");
-                System.out.println();
-            }
-            System.out.println("----");*/
 
-           
+
+
+            return tableView;
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
         
-        return data;
+        return new TableView();
     }
+
+    //засунуть кнопку в ячейку таблицы, не пытаться понять как это работает
+    public Callback<TableColumn<ObservableList, String>, TableCell<ObservableList, String>> getButtonCell(String name){//
+        Callback<TableColumn<ObservableList,String>, TableCell<ObservableList, String>> cellFactory
+                = //
+                new Callback<TableColumn<ObservableList, String>, TableCell<ObservableList, String>>() {
+                    @Override
+                    public TableCell call(final TableColumn<ObservableList, String> param) {
+                        final TableCell<ObservableList, String> cell = new TableCell<ObservableList, String>() {
+
+                            final Button btn = new Button(name);
+                            @Override
+                            public void updateItem(String item, boolean empty) {
+                                super.updateItem(item, empty);
+                                if (empty) {
+                                    setGraphic(null);
+                                    setText(null);
+                                } else {
+
+                                    if(name.equals("изменить")){
+                                        btn.setId("UPDATE");
+                                    }
+
+                                    if(name.equals("удалить")){
+                                        btn.setId("DELETE");
+                                    }
+
+                                    btn.setOnAction(event -> {
+
+                                        String id = (String) getTableView().getItems().get(getIndex()).get(0);
+                                        String requestType = btn.getId();
+                                        String tableName = Controller.currentTable;
+
+                                        String SQLRequest = requestType;
+                                        if(requestType.equals("DELETE")){
+                                            SQLRequest += " from "+tableName+" where id = "+id+";";
+                                        }
+
+                                        System.out.println("SQL request "+SQLRequest);
+                                        SQLRequest(SQLRequest);
+
+
+
+                                    });
+                                    setGraphic(btn);
+                                    setText(null);
+                                }
+                            }
+                        };
+                        return cell;
+                    }
+                };
+        return cellFactory;
+    }
+
+
+
     public void SQLRequest(String request){
         System.out.println("Запрос : "+request);
         try {
